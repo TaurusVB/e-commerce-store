1a19478c995a7db6a9b77217b26aca720aa386c8
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-24 21:23:07 +0200

    created Gallery components for Product page | finish working of Product page functionality

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,8 +1,12 @@
+import { FC } from "react";
+
 import getProduct from "@/actions/getProduct";
 import getProducts from "@/actions/getProducts";
+
 import ProductList from "@/components/ProductList";
+import Gallery from "@/components/gallery";
 import Container from "@/components/ui/Container";
-import { FC } from "react";
+import Info from "@/components/Info";
 
 interface IProductPageProps {
   params: {
@@ -22,11 +26,9 @@ const ProductPage: FC<IProductPageProps> = async ({ params }) => {
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            {/* gallery */}
-            <div>Gallery</div>
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              {/* info */}
-              Infro
+              <Info data={product} />
             </div>
           </div>
           <hr className="my-10" />
