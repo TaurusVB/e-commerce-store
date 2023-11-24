8fd71d663d4120477929c53d76459084b00a5ced
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-24 17:47:40 +0200

    created Billboard components | created getBillboard action

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,17 @@
-const HomePage = () => {
-  return <div>Hello store!</div>;
+import getBillboard from "@/actions/getBillboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
+
+const HomePage = async () => {
+  const billboard = await getBillboard("17124fc6-0521-449b-9f19-bb336a77e1ed");
+
+  return (
+    <Container>
+      <div className=" space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
